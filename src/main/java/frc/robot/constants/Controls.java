075350dc6d426697d3c6e81ef890843f00eeeab0
Jasper Chen <jdchen2017@gmail.com>
2025-02-05 14:29:59 -0600

    most operator controls

--- src/main/java/frc/robot/constants/Controls.java
+++ src/main/java/frc/robot/constants/Controls.java
@@ -35,14 +35,23 @@ public class Controls {
         public static final DoubleSupplier SwerveRotationAxis = OI.getInstance().driverController()::rightStickX;
 
         public static final Trigger precisionTrigger = OI.getInstance().driverController().LEFT_TRIGGER;
-
-        public static final Trigger rotationResetTrigger = OI.getInstance().driverController().A_BUTTON
-                                                            .and(OI.getInstance().driverController().B_BUTTON);
+        // press A and B at the same to reset swerve rotation to the current heading
+        public static final Trigger rotationResetTrigger = OI.getInstance().driverController().A_BUTTON.and(OI.getInstance().driverController().B_BUTTON); 
     }
     public static class Operator{
         //manual override controls
         public static final DoubleSupplier manualElevator = OI.getInstance().operatorController()::leftStickY;
         public static final DoubleSupplier manualWrist = OI.getInstance().operatorController()::rightStickY;
         public static final DoubleSupplier manualScoringOuIntake = OI.getInstance().operatorController()::rightStickX;
+        //manual operator resets
+        public static final Trigger elevatorResetSetPointTrigger = OI.getInstance().operatorController().A_BUTTON.and(OI.getInstance().driverController().B_BUTTON);
+        //normal set points and controls
+        public static final Trigger L4Trigger = OI.getInstance().operatorController().RIGHT_TRIGGER;
+        public static final Trigger L3Trigger = OI.getInstance().operatorController().RIGHT_BUMPER;
+        public static final Trigger L2Trigger = OI.getInstance().operatorController().X_BUTTON;
+        public static final Trigger L1Trigger = OI.getInstance().operatorController().Y_BUTTON;
+        public static final Trigger processorScoreTrigger = OI.getInstance().operatorController().A_BUTTON;
+        public static final Trigger bargeScoreTrigger = OI.getInstance().operatorController().B_BUTTON;
+        public static final Trigger stowElevatorTrigger = OI.getInstance().operatorController().POV_DOWN;
     }
 }
