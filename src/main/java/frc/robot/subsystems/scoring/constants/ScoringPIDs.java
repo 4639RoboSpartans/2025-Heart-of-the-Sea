b3343a0dce14ef74fdbb36379d44301ba76bb0a6
aditya-khambekar <adityakhambekar9@gmail.com>
2025-02-08 14:40:01 -0600

    github is not having fun today

--- src/main/java/frc/robot/subsystems/scoring/constants/ScoringPIDs.java
+++ src/main/java/frc/robot/subsystems/scoring/constants/ScoringPIDs.java
@@ -13,7 +13,6 @@ public class ScoringPIDs {
     public static TunableNumber elevatorKv = new TunableNumber("Elevator Kv").withDefaultValue(0.0);
     public static TunableNumber elevatorKa = new TunableNumber("Elevator Ka").withDefaultValue(0.0);
 
-<<<<<<< HEAD
     public static TunableNumber wristKp = new TunableNumber("Wrist kP").withDefaultValue(100.0);
     public static TunableNumber wristKi = new TunableNumber("Wrist kI").withDefaultValue(0.0);
     public static TunableNumber wristKd = new TunableNumber("Wrist kD").withDefaultValue(2.0);
@@ -23,37 +22,4 @@ public class ScoringPIDs {
     public static TunableNumber wristKg = new TunableNumber("Wrist Kg").withDefaultValue(0.0);
     public static TunableNumber wristKv = new TunableNumber("Wrist Kv").withDefaultValue(0.0);
     public static TunableNumber wristKa = new TunableNumber("Wrist Ka").withDefaultValue(0.0);
-=======
-    public static TunableNumber wristKp = new TunableNumber("Wrist kP");
-    public static TunableNumber wristKi = new TunableNumber("Wrist kI");
-    public static TunableNumber wristKd = new TunableNumber("Wrist kD");
-    public static TunableNumber wristVelocity = new TunableNumber("Wrist Velocity");
-    public static TunableNumber wristAcceleration = new TunableNumber("Wrist Acceleration");
-    public static TunableNumber wristKs = new TunableNumber("Wrist Ks");
-    public static TunableNumber wristKg = new TunableNumber("Wrist Kg");
-    public static TunableNumber wristKv = new TunableNumber("Wrist Kv");
-    public static TunableNumber wristKa = new TunableNumber("Wrist Ka");
-
-    static {
-        elevatorKp.setDefault(0);
-        elevatorKi.setDefault(0);
-        elevatorKd.setDefault(0);
-        elevatorVelocity.setDefault(0.3);
-        elevatorAcceleration.setDefault(0.3);
-        elevatorKs.setDefault(0);
-        elevatorKg.setDefault(0.625);
-        elevatorKv.setDefault(0);
-        elevatorKa.setDefault(0);
-
-        wristKp.setDefault(100.0);
-        wristKi.setDefault(0.0);
-        wristKd.setDefault(2.0);
-        wristVelocity.setDefault(30);
-        wristAcceleration.setDefault(20);
-        wristKs.setDefault(0.0);
-        wristKg.setDefault(0.0);
-        wristKv.setDefault(0.0);
-        wristKa.setDefault(0.0);
-    }
->>>>>>> 8bc0ed923b6486f611454f74519cb33e1e3fbc3c
 }
