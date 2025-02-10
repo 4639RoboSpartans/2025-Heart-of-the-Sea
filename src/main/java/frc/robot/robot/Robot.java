25b3216059576da6512e2ac2ab70b9ebd403fe10
Michael <nengzhimj@gmail.com>
2025-02-10 17:23:14 -0600

    starting work on simulation with custom assets installed akit, modified Robot.java

--- src/main/java/frc/robot/robot/Robot.java
+++ src/main/java/frc/robot/robot/Robot.java
@@ -7,105 +7,124 @@ package frc.robot.robot;
 
 import com.ctre.phoenix6.SignalLogger;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.scoring.ScoringSuperstructure;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
 
 
-
-public class Robot extends TimedRobot
-{
+public class Robot extends LoggedRobot {
     private Command autonomousCommand;
-    
+
     private final RobotContainer robotContainer;
-    
-    
-    public Robot()
-    {
+
+
+    public Robot() {
         SignalLogger.enableAutoLogging(true);
         robotContainer = new RobotContainer();
     }
 
     @Override
     public void robotInit() {
+        Logger.start();
         ScoringSuperstructure.getInstance().setDefaultCommand(ScoringSuperstructure.getInstance().runScoringState());
     }
-    
-    
+
+
     @Override
-    public void robotPeriodic()
-    {
+    public void robotPeriodic() {
         CommandScheduler.getInstance().run();
+        Logger.recordOutput(
+                "Robot Pose", new Pose2d()
+        );
+        Logger.recordOutput(
+                "ZeroedComponentPoses",
+                new Pose3d(),
+                new Pose3d(),
+                new Pose3d(),
+                new Pose3d());
+        Logger.recordOutput(
+                "FinalComponentPoses",
+                new Pose3d(),
+                new Pose3d(),
+                new Pose3d(),
+                new Pose3d());
     }
-    
-    
+
+
     @Override
     public void disabledInit() {
 
     }
-    
-    
+
+
     @Override
-    public void disabledPeriodic() {}
-    
-    
+    public void disabledPeriodic() {
+    }
+
+
     @Override
-    public void disabledExit() {}
-    
-    
+    public void disabledExit() {
+    }
+
+
     @Override
-    public void autonomousInit()
-    {
+    public void autonomousInit() {
         autonomousCommand = robotContainer.getAutonomousCommand();
-        
-        if (autonomousCommand != null)
-        {
+
+        if (autonomousCommand != null) {
             autonomousCommand.schedule();
         }
     }
-    
-    
+
+
     @Override
-    public void autonomousPeriodic() {}
-    
-    
+    public void autonomousPeriodic() {
+    }
+
+
     @Override
-    public void autonomousExit() {}
-    
-    
+    public void autonomousExit() {
+    }
+
+
     @Override
-    public void teleopInit()
-    {
+    public void teleopInit() {
         SignalLogger.start();
-        if (autonomousCommand != null)
-        {
+        if (autonomousCommand != null) {
             autonomousCommand.cancel();
         }
     }
-    
-    
+
+
     @Override
-    public void teleopPeriodic() {}
-    
-    
+    public void teleopPeriodic() {
+    }
+
+
     @Override
     public void teleopExit() {
         SignalLogger.stop();
     }
-    
-    
+
+
     @Override
-    public void testInit()
-    {
+    public void testInit() {
         CommandScheduler.getInstance().cancelAll();
     }
-    
-    
+
+
     @Override
-    public void testPeriodic() {}
-    
-    
+    public void testPeriodic() {
+    }
+
+
     @Override
-    public void testExit() {}
+    public void testExit() {
+    }
 }
