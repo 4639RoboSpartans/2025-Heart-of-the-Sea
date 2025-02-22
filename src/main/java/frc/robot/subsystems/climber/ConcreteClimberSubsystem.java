3392ed5692c330fecd8ffb54c883cb9ae0847900
NJonathanJiang <theNerdyMango@gmail.com>
2025-02-22 12:43:48 -0600

    updated climber subsystems

--- src/main/java/frc/robot/subsystems/climber/ConcreteClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/climber/ConcreteClimberSubsystem.java
@@ -7,41 +7,35 @@ import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkFlexConfig;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.lib.annotation.PackagePrivate;
+import frc.robot.subsystems.climber.ClimberConstants.IDs;
 
 import java.util.function.DoubleSupplier;
 
 @PackagePrivate
 class ConcreteClimberSubsystem extends AbstractClimberSubsystem {
-    @SuppressWarnings("FieldCanBeLocal")
-    private final SparkFlex leftPivot, rightPivot;
+    private final RatchetMotor ratchetMotor;
 
-    // TODO: add PIDs
-
-    @PackagePrivate
-    ConcreteClimberSubsystem() {
-        leftPivot = new SparkFlex(ClimberConstants.IDs.LEFT_CLIMBER_MOTOR, SparkLowLevel.MotorType.kBrushless);
-        rightPivot = new SparkFlex(ClimberConstants.IDs.RIGHT_CLIMBER_MOTOR, SparkLowLevel.MotorType.kBrushless);
-
+    public ConcreteClimberSubsystem() {
+        SparkFlex motor = new SparkFlex(IDs.CLIMBER_MOTOR, SparkLowLevel.MotorType.kBrushless);
         SparkBaseConfig cfg = new SparkFlexConfig()
             .idleMode(SparkBaseConfig.IdleMode.kBrake)
-            .smartCurrentLimit(30); // TODO: use a better value
+            .smartCurrentLimit(30);
+        motor.configure(cfg, SparkBase.ResetMode.kNoResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
 
-        leftPivot.configure(cfg, SparkBase.ResetMode.kNoResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
-        rightPivot.configure(cfg, SparkBase.ResetMode.kNoResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+        ratchetMotor = new RatchetMotor(motor, IDs.CLIMBER_RATCHET_DIO_PORT, RatchetMotor.RatchetDirection.Forwards);
     }
 
 
     @Override
     public Command runClimber(DoubleSupplier speedSupplier) {
         return run(
-            () -> setMotors(speedSupplier.getAsDouble())
+            () -> setMotor(speedSupplier.getAsDouble())
         ).finallyDo(
-            () -> setMotors(0)
+            () -> setMotor(0)
         );
     }
 
-    private void setMotors(double speedSupplier) {
-        leftPivot.set(speedSupplier);
-        rightPivot.set(speedSupplier);
+    private void setMotor(double speed) {
+        ratchetMotor.set(speed);
     }
 }
