d42eb62a88e1ad13adbbf44384821112c4ba1906
NJonathanJiang <theNerdyMango@gmail.com>
2025-02-23 16:46:52 -0600

    Limelight fix

--- src/main/java/frc/robot/constants/Limelights.java
+++ src/main/java/frc/robot/constants/Limelights.java
@@ -2,7 +2,7 @@ package frc.robot.constants;
 
 public enum Limelights {
     LIMELIGHT("limelight"),
-    LIMELIGHT2("limelight-slhs");
+    LIMELIGHT2("slhslimelight");
 
     final String name;
     private Limelights(String name){
