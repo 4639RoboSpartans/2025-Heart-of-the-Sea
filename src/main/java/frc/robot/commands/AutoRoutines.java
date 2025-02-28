7670217fdc86ec8b5b53990cf23fe3bccc4b5b1b
NJonathanJiang <theNerdyMango@gmail.com>
2025-02-28 13:31:35 -0600

    refactored AutoRoutines

--- src/main/java/frc/robot/commands/AutoRoutines.java
+++ src/main/java/frc/robot/commands/AutoRoutines.java
@@ -99,83 +99,91 @@ public class AutoRoutines {
         ScoringTarget... scoringTargets
         ///List<Integer> scoringHeights, List<Character> scoringLocations
     ) {
-        String pathPrefix = isComp ? "COMP-" : "TEST-";
-        String name = getAutonName(scoringTargets, pathPrefix);
-        String pathName = getAutonPathName(scoringTargets, pathPrefix);
+        // Create names
+        String namePrefix = isComp ? "COMP-" : "TEST-";
+        String autonName = getAutonName(scoringTargets, namePrefix);
+        String autonPathName = getAutonPathName(scoringTargets, namePrefix);
 
-        int numScores = scoringTargets.length;
-        int numPaths = (numScores) // Movements to reef
-            + (numScores - 1);     // Movements to HP station
+        // Create path segments
+        int numPathSegments = (scoringTargets.length) + (scoringTargets.length - 1);     // Movements to HP station
+        AutoRoutine routine = factory.newRoutine(autonName);
+        AutoTrajectory[] pathSegments = IntStream.range(0, numPathSegments)
+            .mapToObj(i -> routine.trajectory(autonPathName, i))
+            .toArray(AutoTrajectory[]::new);
 
-        AutoRoutine routine = factory.newRoutine(name);
-        List<AutoTrajectory> paths = IntStream.range(0, numPaths)
-            .mapToObj(i -> routine.trajectory(pathName, i))
-            .toList();
-
+        // Create commands
         List<Command> commands = new ArrayList<>();
-        commands.add(paths.get(0).resetOdometry());
-
-        for (int pathIndex = 0; pathIndex < numPaths; pathIndex++) {
-            commands.add(paths.get(pathIndex).cmd());
-            Pose2d targetPose;
-            AbstractSwerveDrivetrain drivetrain = SubsystemManager.getInstance().getDrivetrain();
-            if (pathIndex % 2 == 0) {
-                targetPose = (
-                    scoringTargets[pathIndex / 2].getTargetPosition().getPose()
-                );
-                commands.add(
-                    drivetrain
-                        .directlyMoveTo(targetPose)
-                        .until(
-                            () -> PoseUtil.withinTolerance(
-                                targetPose,
-                                drivetrain.getPose(),
-                                Units.inchesToMeters(0.5))
-                        )
-                        .andThen(scoringTargets[pathIndex / 2].getTargetPosition().fineTuneTargetCommand.get()));
-            } else {
-                targetPose = (
-                    TargetPositions.CORALSTATION_LEFT.getPose()
-                );
-                commands.add(
-                    drivetrain
-                        .directlyMoveTo(targetPose)
-                        .until(
-                            () -> PoseUtil.withinTolerance(
-                                targetPose,
-                                drivetrain.getPose(),
-                                Units.inchesToMeters(0.5))
-                        )
-                );
+        commands.add(pathSegments[0].resetOdometry());
+        for (int targetIndex = 0; targetIndex < scoringTargets.length; targetIndex++) {
+            addScoringSegment(commands, pathSegments[targetIndex * 2], scoringTargets[targetIndex]);
+            if (targetIndex != scoringTargets.length - 1) {
+                addHPLoadingSegment(commands, pathSegments[targetIndex * 2 + 1]);
             }
-
-
-            commands.add(
-                pathIndex % 2 == 0 ?
-                    switch (scoringTargets[pathIndex / 2].scoringHeight()) {
-                        case 1 -> AutoCommands.L1Score.get();
-                        case 2 -> AutoCommands.L2Score.get();
-                        case 3 -> AutoCommands.L3Score.get();
-                        case 4 -> AutoCommands.L4Score.get();
-                        default -> AutoCommands.HPLoad.get().withTimeout(1);
-                    } : AutoCommands.HPLoad.get().withTimeout(1)
-            );
         }
 
-        routine.active().onTrue(
-            CommandsUtil.sequence(commands)
-        );
+        // Activate the commands when the auton routine is active
+        routine.active().onTrue(CommandsUtil.sequence(commands));
+
         return routine;
     }
 
-    private static String getAutonPathName(ScoringTarget[] scoringTargets, String pathPrefix) {
-        return pathPrefix + Arrays.stream(scoringTargets)
+    private void addScoringSegment(List<Command> commands, AutoTrajectory path, ScoringTarget scoringTarget) {
+        // Add path to scoring
+        commands.add(path.cmd());
+
+        // Add directly move to and fine tune stuff IDK
+        TargetPositions targetPosition = scoringTarget.getTargetPosition();
+        addDirectlyMoveToCommand(
+            commands, targetPosition.getPose()
+        );
+        commands.add(targetPosition.fineTuneTargetCommand.get());
+
+        // Add scoring command
+        commands.add(switch (scoringTarget.scoringHeight()) {
+            case 1 -> AutoCommands.L1Score.get();
+            case 2 -> AutoCommands.L2Score.get();
+            case 3 -> AutoCommands.L3Score.get();
+            case 4 -> AutoCommands.L4Score.get();
+            default -> AutoCommands.HPLoad.get().withTimeout(1);
+        });
+    }
+
+    private void addHPLoadingSegment(List<Command> commands, AutoTrajectory path) {
+        // Add path to intake
+        commands.add(path.cmd());
+
+        // Add directly move to stuff IDK
+        addDirectlyMoveToCommand(
+            commands, TargetPositions.CORALSTATION_LEFT.getPose()
+        );
+
+        // Add HP load command
+        commands.add(AutoCommands.HPLoad.get().withTimeout(1));
+    }
+
+    private static void addDirectlyMoveToCommand(List<Command> commands, Pose2d targetPose) {
+        AbstractSwerveDrivetrain drivetrain = SubsystemManager.getInstance().getDrivetrain();
+
+        commands.add(
+            drivetrain
+                .directlyMoveTo(targetPose)
+                .until(
+                    () -> PoseUtil.withinTolerance(
+                        targetPose,
+                        drivetrain.getPose(),
+                        Units.inchesToMeters(0.5))
+                )
+        );
+    }
+
+    private static String getAutonPathName(ScoringTarget[] scoringTargets, String namePrefix) {
+        return namePrefix + Arrays.stream(scoringTargets)
             .map(i -> String.valueOf(i.scoringLocation))
             .collect(Collectors.joining("-"));
     }
 
-    private static String getAutonName(ScoringTarget[] scoringTargets, String pathPrefix) {
-        return pathPrefix + Arrays.stream(scoringTargets)
+    private static String getAutonName(ScoringTarget[] scoringTargets, String namePrefix) {
+        return namePrefix + Arrays.stream(scoringTargets)
             .map(ScoringTarget::toString)
             .collect(Collectors.joining("-"));
     }
