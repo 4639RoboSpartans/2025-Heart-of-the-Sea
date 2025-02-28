fa5edd65bb96678e9e66c0a903e7fd406c45bd28
NJonathanJiang <theNerdyMango@gmail.com>
2025-02-28 12:41:23 -0600

    slight fixes in consistency

--- src/main/java/frc/robot/commands/AutoRoutines.java
+++ src/main/java/frc/robot/commands/AutoRoutines.java
@@ -1,22 +1,20 @@
 package frc.robot.commands;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
-
 import choreo.auto.AutoFactory;
 import choreo.auto.AutoRoutine;
 import choreo.auto.AutoTrajectory;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.lib.util.AllianceFlipUtil;
 import frc.lib.util.CommandsUtil;
 import frc.lib.util.PoseUtil;
 import frc.robot.constants.FieldConstants;
 import frc.robot.subsystems.SubsystemManager;
 import frc.robot.subsystems.drive.AbstractSwerveDrivetrain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class AutoRoutines {
     private final AutoFactory factory;
@@ -27,49 +25,49 @@ public class AutoRoutines {
 
     public AutoRoutine COMP_J_K() {
         return compileAuton(
-                List.of('J', 'K'),
-                List.of(4, 4),
-                true
+            List.of('J', 'K'),
+            List.of(4, 4),
+            true
         );
     }
 
     public AutoRoutine COMP_H_G() {
         return compileAuton(
-                List.of('H', 'G'),
-                List.of(4, 4),
-                true
+            List.of('H', 'G'),
+            List.of(4, 4),
+            true
         );
     }
 
     public AutoRoutine COMP_A_B() {
         return compileAuton(
-                List.of('A', 'B'),
-                List.of(4, 4),
-                true
+            List.of('A', 'B'),
+            List.of(4, 4),
+            true
         );
     }
 
     public AutoRoutine COMP_H_A() {
         return compileAuton(
-                List.of('H', 'A'),
-                List.of(4, 4),
-                true
+            List.of('H', 'A'),
+            List.of(4, 4),
+            true
         );
     }
 
     public AutoRoutine COMP_G_C_D_B() {
         return compileAuton(
-                List.of('G', 'C', 'D', 'B'),
-                List.of(4, 4, 4, 4),
-                true
+            List.of('G', 'C', 'D', 'B'),
+            List.of(4, 4, 4, 4),
+            true
         );
     }
 
     public AutoRoutine TEST_A_B() {
         return compileAuton(
-                List.of('A', 'B'),
-                List.of(1, 2),
-                false
+            List.of('A', 'B'),
+            List.of(1, 2),
+            false
         );
     }
 
@@ -79,12 +77,15 @@ public class AutoRoutines {
      * @param scoringHeights   all heights, each corresponding to a location,
      *                         must be the same size as {@param scoringLocations}
      * @param comp             is the auton for competition
+     *
      * @return a new routine with the specified characteristics
      */
     private AutoRoutine compileAuton(List<Character> scoringLocations, List<Integer> scoringHeights, boolean comp) {
         StringBuilder pathName = new StringBuilder(comp ? "COMP-" : "TEST-");
         StringBuilder name = new StringBuilder(pathName.toString());
-        int numPaths = scoringHeights.size() * 2 - 1;
+        int numPaths = (scoringHeights.size()) // Movements to reef
+            + (scoringHeights.size() - 1)      // Movements to HP station
+            ;
         int numScores = scoringLocations.size();
         for (int i = 0; i < numScores; i++) {
             pathName.append(scoringLocations.get(i));
@@ -95,78 +96,80 @@ public class AutoRoutines {
                 name.append("-");
             }
         }
+
         AutoRoutine routine = factory.newRoutine(name.toString());
-        ArrayList<AutoTrajectory> paths = new ArrayList<>();
+        List<AutoTrajectory> paths = new ArrayList<>();
         for (int i : IntStream.range(0, numPaths).toArray()) {
             paths.add(routine.trajectory(pathName.toString(), i));
         }
+
         List<Command> commands = new ArrayList<>();
         commands.add(paths.get(0).resetOdometry());
-        for (int i = 0; i < numPaths; i++) {
-            commands.add(paths.get(i).cmd());
-            int finalI = i;
+
+        for (int pathIndex = 0; pathIndex < numPaths; pathIndex++) {
+            commands.add(paths.get(pathIndex).cmd());
             Pose2d targetPose;
             AbstractSwerveDrivetrain drivetrain = SubsystemManager.getInstance().getDrivetrain();
-            if (i % 2 == 0) {
+            if (pathIndex % 2 == 0) {
                 targetPose = (
-                        FieldConstants.TargetPositions.valueOf(
-                                "REEF_" + scoringLocations.get(i / 2)
-                        ).getPose()
+                    FieldConstants.TargetPositions.valueOf(
+                        "REEF_" + scoringLocations.get(pathIndex / 2)
+                    ).getPose()
                 );
                 commands.add(
-                        drivetrain
-                                .directlyMoveTo(targetPose)
-                                .until(
-                                        () -> PoseUtil.withinTolerance(
-                                                targetPose,
-                                                drivetrain.getPose(),
-                                                Units.inchesToMeters(0.5))
-                                )
-                .andThen(FieldConstants.TargetPositions.valueOf(
-                        "REEF_" + scoringLocations.get(i / 2)
-                ).fineTuneTargetCommand.get()));
+                    drivetrain
+                        .directlyMoveTo(targetPose)
+                        .until(
+                            () -> PoseUtil.withinTolerance(
+                                targetPose,
+                                drivetrain.getPose(),
+                                Units.inchesToMeters(0.5))
+                        )
+                        .andThen(FieldConstants.TargetPositions.valueOf(
+                            "REEF_" + scoringLocations.get(pathIndex / 2)
+                        ).fineTuneTargetCommand.get()));
             } else {
                 targetPose = (
-                        FieldConstants.TargetPositions.CORALSTATION_LEFT.getPose()
+                    FieldConstants.TargetPositions.CORALSTATION_LEFT.getPose()
                 );
                 commands.add(
-                        drivetrain
-                                .directlyMoveTo(targetPose)
-                                .until(
-                                        () -> PoseUtil.withinTolerance(
-                                                targetPose,
-                                                drivetrain.getPose(),
-                                                Units.inchesToMeters(0.5))
-                                )
+                    drivetrain
+                        .directlyMoveTo(targetPose)
+                        .until(
+                            () -> PoseUtil.withinTolerance(
+                                targetPose,
+                                drivetrain.getPose(),
+                                Units.inchesToMeters(0.5))
+                        )
                 );
             }
 
 
             commands.add(
-                    i % 2 == 0 ?
-                            switch (scoringHeights.get(i / 2)) {
-                                case 1 -> AutoCommands.L1Score.get();
-                                case 2 -> AutoCommands.L2Score.get();
-                                case 3 -> AutoCommands.L3Score.get();
-                                case 4 -> AutoCommands.L4Score.get();
-                                default -> AutoCommands.HPLoad.get().alongWith(drivetrain.stop()).withTimeout(1);
-                            } : AutoCommands.HPLoad.get().alongWith(drivetrain.stop()).withTimeout(1)
+                pathIndex % 2 == 0 ?
+                    switch (scoringHeights.get(pathIndex / 2)) {
+                        case 1 -> AutoCommands.L1Score.get();
+                        case 2 -> AutoCommands.L2Score.get();
+                        case 3 -> AutoCommands.L3Score.get();
+                        case 4 -> AutoCommands.L4Score.get();
+                        default -> AutoCommands.HPLoad.get().withTimeout(1);
+                    } : AutoCommands.HPLoad.get().withTimeout(1)
             );
 
         }
         routine.active().onTrue(
-                CommandsUtil.sequence(commands)
+            CommandsUtil.sequence(commands)
         );
         return routine;
     }
 
     public List<AutoRoutine> getAllCompRoutines() {
         return List.of(
-                COMP_A_B(),
-                COMP_H_A(),
-                COMP_H_G(),
-                COMP_J_K(),
-                COMP_G_C_D_B()
+            COMP_A_B(),
+            COMP_H_A(),
+            COMP_H_G(),
+            COMP_J_K(),
+            COMP_G_C_D_B()
         );
     }
 
