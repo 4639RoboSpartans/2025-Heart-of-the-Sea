752d2fb8da1834630f1d043ec4cb3dfc03b61b91
Michael <nengzhimj@gmail.com>
2025-03-07 17:33:56 -0600

    added comment for how to change alliance side correctly in simulation (otherwise it bugs out)

--- src/main/java/frc/lib/util/DriverStationUtil.java
+++ src/main/java/frc/lib/util/DriverStationUtil.java
@@ -3,7 +3,14 @@ package frc.lib.util;
 import edu.wpi.first.wpilibj.DriverStation;
 
 public class DriverStationUtil {
+    //in simulation, change this before starting the simulation as this is what choreo reads as whether to flip alliance.
+    private static boolean defaultBlue = false;
+
     public static DriverStation.Alliance getAlliance() {
-        return DriverStation.getAlliance().orElse(DriverStation.Alliance.Blue);
+        return DriverStation.getAlliance().orElse(
+                defaultBlue
+                ? DriverStation.Alliance.Blue
+                : DriverStation.Alliance.Red
+        );
     }
 }
