b75c4afaf344a957a2619762a74cef3bd6281125
Michael <nengzhimj@gmail.com>
2025-03-07 09:28:07 -0600

    added limelight fiducial filtering based the apriltag's distance to the robot

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -2,17 +2,21 @@ package frc.robot.subsystems.vision;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.lib.limelight.LimelightHelpers;
 import frc.lib.limelight.data.PoseEstimate;
 import frc.lib.limelight.data.PoseEstimate.Botpose;
+import frc.lib.limelight.data.RawFiducial;
 import frc.lib.tunable.TunableNumber;
 import frc.robot.constants.FieldConstants;
 import frc.robot.constants.Limelights;
 import frc.robot.subsystems.drive.AbstractSwerveDrivetrain;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.OptionalDouble;
 
 import com.ctre.phoenix6.Utils;
@@ -30,6 +34,14 @@ public class Vision {
         if ((RobotBase.isReal())) {
             Arrays.stream(Limelights.values()).parallel().forEach(
                 limelight -> {
+                    if (RobotState.isAutonomous()) {
+                        filterFiducials(limelight);
+                    } else {
+                        LimelightHelpers.setFiducialIDFiltersOverride(
+                            limelight.getName(), 
+                            new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22}
+                        );
+                    }
                     PoseEstimate measurement = 
                             DriverStation.getAlliance().orElse(DriverStation.Alliance.Blue) == DriverStation.Alliance.Blue
                                     ? LimelightHelpers.getBotPoseEstimate(limelight.getName(), Botpose.BLUE_MEGATAG1)
@@ -40,7 +52,13 @@ public class Vision {
 
                     if (res != null) {
                         double[] stdevs = LimelightHelpers.getStDevs_MT1(limelight.getName());
-                        drivetrain.setVisionStandardDeviations(stdevs[0] * 100, stdevs[1] * 100, stdevs[3]);
+                        if (RobotState.isAutonomous()) {
+                            drivetrain.setVisionStandardDeviations(10, 10, 999999);
+                        } else if (RobotState.isDisabled()) {
+                            drivetrain.setVisionStandardDeviations(stdevs[0], stdevs[1], stdevs[3]);
+                        } else {
+                            drivetrain.setVisionStandardDeviations(stdevs[0] * 100, stdevs[1] * 100, stdevs[3]);
+                        }
                         drivetrain.addVisionMeasurement(res, Utils.getCurrentTimeSeconds());
                         visionMeasurements.getObject(limelight.getName()).setPose(res);
                         SmartDashboard.putBoolean("Added Vision", true);
@@ -50,6 +68,30 @@ public class Vision {
                     SmartDashboard.putData("Vision Measurements", visionMeasurements);
                 }
             );
+        }
+    }
+
+    public static void filterFiducials(Limelights limelight) {
+        PoseEstimate measurement = 
+                DriverStation.getAlliance().orElse(DriverStation.Alliance.Blue) == DriverStation.Alliance.Blue
+                        ? LimelightHelpers.getBotPoseEstimate(limelight.getName(), Botpose.BLUE_MEGATAG1)
+                        : LimelightHelpers.getBotPoseEstimate(limelight.getName(), Botpose.RED_MEGATAG1);
+        var res = measurement.pose().getX() == 0 && measurement.pose().getY() == 0
+                ? null
+                : measurement.pose();
+
+        if (res != null) {
+            var rawFiducials = measurement.rawFiducials();
+            List<RawFiducial> allFiducials = new ArrayList<>();
+            for (RawFiducial fiducial : rawFiducials) allFiducials.add(fiducial);
+            List<Integer> allIds = allFiducials.stream().filter(
+                fiducial -> fiducial.distToCamera() >= distanceThreshold.get()
+            ).map(RawFiducial::id).toList();
+            int[] allIdInts = new int[allIds.size()];
+            for (int i = 0; i < allIds.size(); i++) {
+                allIdInts[i] = allIds.get(i);
+            }
+            LimelightHelpers.setFiducialIDFiltersOverride(limelight.getName(), allIdInts);
         }
     }
 
